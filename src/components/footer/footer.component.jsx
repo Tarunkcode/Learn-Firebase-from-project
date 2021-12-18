447ae5bbd8a7aec13c5b9583e6b18c04615eeb5b
Tarunkcode <68896998+Tarunkcode@users.noreply.github.com>
2021-12-18 21:06:59 +0530

    add footer component

---
+++ src/components/footer/footer.component.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './footer.styles.scss';
+
+const Footer =() => (
+    <div className='footer'>
+        <h3>All right reserved @2021 by owner of this project TARUN KUMAR.</h3>
+    </div>
+)
+export default Footer;
